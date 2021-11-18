25e2cfa5e089f115e929c0fee10583553519a30d
Gabriel Cavalcante <gabrielcalk@outlook.com>
2021-11-18 17:38:00 -0500

    added the data storage on indexdb on mongodb after the window is online

--- public/js/db.js
+++ public/js/db.js
@@ -8,7 +8,11 @@ const request = window.indexedDB.open(dbName, 1);
 
 request.onsuccess = function (event) {
     db = event.target.result;
-    console.log(event, db)
+
+    if (navigator.onLine) {
+        console.log('Backend online! 🗄️');
+        syncIndexdb();
+    }
 };
 
 request.onerror = (event) =>{
@@ -41,3 +45,31 @@ const saveRecord = (record) => {
         console.log('Transaction Error', event);
     }
 };
+
+const syncIndexdb = () =>{
+    let transactionGet = db.transaction([storeName], 'readwrite');
+    const objectStore = transactionGet.objectStore(storeName);
+    const getAll = objectStore.getAll();
+
+    getAll.onsuccess = function (){
+        if (getAll.result.length > 0){
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                  Accept: 'application/json, text/plain, */*',
+                  'Content-Type': 'application/json',
+                },
+            })
+            .then(data => data.json())
+            .then(res => {
+            transactionGet = db.transaction([storeName], 'readwrite');
+            const currentStore = transactionGet.objectStore(storeName);
+            currentStore.clear();
+            console.log('Clearing store 🧹');
+            })
+        }
+    }
+};
+
+window.addEventListener('online', syncIndexdb);
