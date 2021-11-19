46246d591590dc453ed7b986c3f8ba7be7b2178d
Gabriel Cavalcante Causin <89816900+gabrielcalk@users.noreply.github.com>
2021-11-18 19:23:16 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,7 @@
+// Checking if the browser support the service worker
 if ('serviceWorker' in navigator){
     window.addEventListener('load', () =>{
+// Registering the service worker
       navigator.serviceWorker.register('/sw.js',).then((registration) =>{
         console.log('ServiceWorket Registration Successful', registration)
       }, function (err){
@@ -7,9 +9,11 @@ if ('serviceWorker' in navigator){
       }
       )
     })
-  }
+}
 
-const cacheItems = 'items_v1'
+// variables that contains the name of the variables
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 // informations that will be on the cacheItems
 const resourcesToCache = [
@@ -26,22 +30,49 @@ const resourcesToCache = [
 // open on cache after the service worker is installed
 self.addEventListener('install', (event) =>{
     event.waitUntil(
-        caches.open(cacheItems)
+        caches.open(CACHE_NAME)
         .then(cache => (cache.addAll(resourcesToCache))),
     );
     event.waitUntil(
-        caches.open(cacheItems)
+        caches.open(DATA_CACHE_NAME)
         .then(cache => (cache.add("/api/transaction"))),
     );
 });
 
-self.addEventListener('fetch', function(event) {
-// inform to the browser to check future events 
+self.addEventListener("fetch", function(event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
     event.respondWith(
-// take the current request and looks in the cache for a resource that matches
-      caches.match(event.request).then(function(response) {
-// checking if the files exist or not on the cache
-        return response || fetch(event.request);
+      fetch(event.request).catch(function() {
+        return caches.match(event.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (event.request.headers.get("accept").includes("text/html")) {
+            // return the cached home page for all requests for html pages
+            return caches.match("/");
+          }
+        });
       })
-    )
-});
+    );
+  });
